b4b5d947d3aee7d9a46fc873f611be1cfad305ba
DINESH <dinesh3212001@gmail.com>
2026-06-24 23:35:44 +0530

    add the __pycache to the .gitignore and created the create_posts method in post requests

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
-
+from fastapi import Body
 app=FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to my FastAPI application!"}
+
+@app.get("/posts")
+async def get_posts():
+    return {"message": "Here are the posts."}
+
+@app.post("/create_posts")
+async def create_post(payload: dict=Body(...)):
+    print(payload)
+    return {"message": "Post created successfully."}
