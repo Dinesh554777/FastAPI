280de82ff01d8d927b395348bedfcc242c9478ed
DINESH <dinesh3212001@gmail.com>
2026-06-26 13:17:39 +0530

    Refactor create_post endpoint to use Post model for request validation and improve response format

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 app=FastAPI()
+
+
+class Post(BaseModel):
+    title:str
+    content:str
+    published: bool = True
+    rating : Optional[int] = None
+    
 
 @app.get("/")
 async def root():
@@ -11,6 +21,8 @@ async def get_posts():
     return {"message": "Here are the posts."}
 
 @app.post("/create_posts")
-async def create_post(payload: dict=Body(...)):
-    print(payload)
-    return {"new_post": f"title: {payload['title']}  content: {payload['content']}" }
+async def create_post(post: Post):
+    
+    print(post.dict())
+    
+    return {"data":post}
