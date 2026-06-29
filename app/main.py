bbfd668e1f7c238b778ea1543f5665f8e74f4698
DINESH <dinesh3212001@gmail.com>
2026-06-29 12:36:18 +0530

    Implement SQLAlchemy integration for posts and update Post model attributes

--- app/main.py
+++ app/main.py
@@ -39,7 +39,9 @@ async def root():
 
 @app.get("/sqlalchemy")
 def test_posts(db: Session = Depends(get_db)):
-    return {"message":"success"}
+    posts= db.query(models.Post).all()
+
+    return {"data":posts}
 
 
 my_posts=[{"title": "My posts", "content":"My posts content", "id": 3},{"title": "My foods", "content":"My favourite foods", "id": 1},{"title": "My cars", "content":"My favourite cars", "id": 5}]
@@ -55,17 +57,25 @@ def find_index_of_posts(id):
             return i
 
 @app.get("/posts")
-async def get_posts():
-    cursor.execute("""SELECT * FROM posts""")
-    posts = cursor.fetchall()
+async def get_posts(db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts""")
+    # posts = cursor.fetchall()
+    posts= db.query(models.Post).all()
+
     return {"data": posts}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-async def create_post(post: Post):
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES(%s,%s,%s) RETURNING *""",(post.title, post.content, post.published))
+async def create_post(post: Post, db: Session = Depends(get_db)):
+    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES(%s,%s,%s) RETURNING *""",(post.title, post.content, post.published))
     
-    new_post=cursor.fetchone()
-    conn.commit()
+    # new_post=cursor.fetchone()
+    # conn.commit()
+    
+    # new_post=models.Post(title=post.title, content=post.content, published=post.published)
+    new_post=models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
 
     
     return {"data":new_post}
