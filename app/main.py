5a0992922817e18c6bf538f7db00a019406cfdeb
DINESH <dinesh3212001@gmail.com>
2026-06-29 13:09:57 +0530

    Refactor update_post to use SQLAlchemy for database operations and improve error handling in delete_posts

--- app/main.py
+++ app/main.py
@@ -104,18 +104,23 @@ def delete_posts(id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
     post.delete(synchronize_session=False)
     db.commit()
-    
+
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post):
-    cursor.execute("""UPDATE posts SET title = %s, content =%s,published=%s WHERE id =%s RETURNING *""",(post.title,post.content,post.published, str(id)))
+def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
+    # cursor.execute("""UPDATE posts SET title = %s, content =%s,published=%s WHERE id =%s RETURNING *""",(post.title,post.content,post.published, str(id)))
 
-    updated_post=cursor.fetchone()
-    conn.commit()
+    # updated_post=cursor.fetchone()
+    # conn.commit()
+
+    post_query=db.query(models.Post).filter(models.Post.id == id)
+    post=post_query.first()
    
      
-    if updated_post==None:
+    if post==None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
-     
-    return {"message" : updated_post}
+    
+    post_query.update(updated_post.dict(),synchronize_session=False)
+    db.commit()
+    return {"message" :post_query.first()}
