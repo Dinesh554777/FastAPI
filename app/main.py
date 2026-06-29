d5bf9fddfacf163ac99b1bfc765a751fa4866eb7
DINESH <dinesh3212001@gmail.com>
2026-06-29 15:47:43 +0530

    Add password hashing utility and integrate it into user creation endpoint

--- app/main.py
+++ app/main.py
@@ -7,9 +7,8 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
-from . import models, schemas
+from . import models, schemas, utils
 from .database import engine, get_db
-
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -117,6 +116,14 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
 
 @app.post("/users",status_code=status.HTTP_201_CREATED,response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate,db: Session = Depends(get_db)):
+    print(user.password)
+    print(len(user.password))
+    print(type(user.password))
+
+    # hashed_password = pwd_context.hash(user.password)
+    hashed_password=utils.hash(user.password)
+    user.password=hashed_password
+    
     new_user=models.User(**user.dict())
     db.add(new_user)
     db.commit()
