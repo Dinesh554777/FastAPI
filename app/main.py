8346dc1577afa06fa71e88046338b73aee6a2895
DINESH <dinesh3212001@gmail.com>
2026-06-30 14:49:46 +0530

    Add authentication router and user login schema with verification utility

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ import time
 from sqlalchemy.orm import Session
 from . import models, schemas, utils
 from .database import engine, get_db
-from .routers import user, post
+from .routers import user, post, auth
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -47,5 +47,5 @@ def find_index_of_posts(id):
 
 app.include_router(post.router)
 app.include_router(user.router)
-
+app.include_router(auth.router)
 
